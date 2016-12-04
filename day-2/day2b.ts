1518f5d5b5bc6df4f94b7881bd8f884d47b75b2d
Freek Wielstra <fwielstra@xebia.com>
2016-12-04 15:06:50 +0100

    Implement solution for day 2b

--- day-2/day2b.ts
+++ day-2/day2b.ts
@@ -1,8 +1,10 @@
-module day2 {
+module day2b {
   const keypad = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [0, 0, 1, 0, 0],
+    [0, 2, 3, 4, 0],
+    [5, 6, 7, 8, 9],
+    [0, 'A', 'B', 'C', 0],
+    [0, 0, 'D', 0, 0]
   ]
 
   const demoInstructions =
@@ -18,23 +20,28 @@ UDUUUUURUDLLLRRRDRDRUDDRLLDRRLDRLLUURRULUULULRLLRUDDRLDRLUURDUDLURUULLLULLRRRULR
 ULRDULURRDDLULLDDLDDDRLDUURDLLDRRRDLLURDRUDDLDURUDRULRULRULULUULLLLDRLRLDRLLLLLRLRRLRLRRRDDULRRLUDLURLLRLLURDDRRDRUUUDLDLDRRRUDLRUDDRURRDUUUDUUULRLDDRDRDRULRLLDLDDLLRLUDLLLLUURLDLRUDRLRDRDRLRULRDDURRLRUDLRLRLDRUDURLRDLDULLUUULDRLRDDRDUDLLRUDDUDURRRRDLDURRUURDUULLDLRDUDDLUDDDRRRULRLULDRLDDRUURURLRRRURDURDRULLUUDURUDRDRLDLURDDDUDDURUDLRULULURRUULDRLDULRRRRDUULLRRRRLUDLRDDRLRUDLURRRDRDRLLLULLUULRDULRDLDUURRDULLRULRLRRURDDLDLLRUUDLRLDLRUUDLDDLLULDLUURRRLRDULRLRLDRLDUDURRRLLRUUDLUURRDLDDULDLULUUUUDRRULLLLLLUULDRULDLRUDDDRDRDDURUURLURRDLDDRUURULLULUUUDDLRDULDDLULDUDRU
 LRLRLRLLLRRLUULDDUUUURDULLLRURLDLDRURRRUUDDDULURDRRDURLRLUDLLULDRULLRRRDUUDDRDRULLDDULLLUURDLRLRUURRRLRDLDUDLLRLLURLRLLLDDDULUDUDRDLRRLUDDLRDDURRDRDUUULLUURURLRRDUURLRDLLUDURLRDRLURUURDRLULLUUUURRDDULDDDRULURUULLUDDDDLRURDLLDRURDUDRRLRLDLRRDDRRDDRUDRDLUDDDLUDLUDLRUDDUDRUDLLRURDLRUULRUURULUURLRDULDLDLLRDRDUDDDULRLDDDRDUDDRRRLRRLLRRRUUURRLDLLDRRDLULUUURUDLULDULLLDLULRLRDLDDDDDDDLRDRDUDLDLRLUDRRDRRDRUURDUDLDDLUDDDDDDRUURURUURLURLDULUDDLDDLRUUUULRDRLUDLDDLLLRLLDRRULULRLRDURRRLDDRDDRLU`
 
-  let pos = [1, 1]
-
-  const mutators = {
-    'U': () => pos[0] = Math.max(pos[0] - 1, 0),
-    'D': () => pos[0] = Math.min(pos[0] + 1, 2),
-    'L': () => pos[1] = Math.max(pos[1] - 1, 0),
-    'R': () => pos[1] = Math.min(pos[1] + 1, 2)
-  }
+  let pos = [2, 0]
 
   const getNumberForLine = line => {
-    line.split('').forEach(direction => mutators[direction]())
+    line.split('').forEach(direction => {
+      let newpos = [pos[0], pos[1]]
+      switch(direction) {
+        case 'U': newpos[0] = Math.max(pos[0] - 1, 0); break
+        case 'D': newpos[0] = Math.min(pos[0] + 1, 4); break
+        case 'L': newpos[1] = Math.max(pos[1] - 1, 0); break
+        case 'R': newpos[1] = Math.min(pos[1] + 1, 4); break
+      }
+
+      if (keypad[newpos[0]][newpos[1]] !== 0) {
+        pos = [newpos[0], newpos[1]]
+      }
+    })
     return keypad[pos[0]][pos[1]]
   }
 
   const getCode = data => data.split('\n').map(getNumberForLine).join('')
 
-  console.log('demo', getCode(demoInstructions))
-  pos = [1, 1]
-  console.log('4 realz', getCode(realInstructions))
+  console.log('demo', getCode(demoInstructions)) // 5DB3
+  pos = [2, 0]
+  console.log('4 realz', getCode(realInstructions)) // B3C27
 }
